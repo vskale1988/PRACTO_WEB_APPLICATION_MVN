5a287d2340f276f2e2add736fd11a248fb772ab3
vskale <vskalepatil@gmail.com>
2020-09-29 15:12:34 +0530

    first commit

---
+++ src/main/java/com/practo/PractoWebApplicationMvnApplication.java
@@ -0,0 +1,13 @@
+package com.practo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PractoWebApplicationMvnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PractoWebApplicationMvnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practo/PractoWebApplicationMvnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PractoWebApplicationMvnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

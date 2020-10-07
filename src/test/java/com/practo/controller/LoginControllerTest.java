c1d3e17dffc71f9f31a0b2e2f9601ddc9ebf64ab
vskale <vskalepatil@gmail.com>
2020-10-07 19:56:08 +0530

    add test case

---
+++ src/test/java/com/practo/controller/LoginControllerTest.java
@@ -0,0 +1,14 @@
+package com.practo.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LoginControllerTest {
+
+	@Test
+	public void testValidWebUser() {
+		fail("Not yet implemented");
+	}
+
+}

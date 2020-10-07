557208a83646bf0dff5b6c3f0f001b218bf1dda7
vskale <vskalepatil@gmail.com>
2020-10-07 20:19:31 +0530

    update test case

--- src/test/java/com/practo/controller/LoginControllerTest.java
+++ src/test/java/com/practo/controller/LoginControllerTest.java
@@ -1,14 +1,28 @@
 package com.practo.controller;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Optional;
 
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.practo.model.UserDto;
 
 public class LoginControllerTest {
-
+	@Autowired
+	LoginController loginController;
 	@Test
 	public void testValidWebUser() {
-		fail("Not yet implemented");
+		UserDto userDto = new UserDto();
+		userDto.setUsername("vijay");
+		userDto.setPassword("kale");
+		Optional<UserDto> user = Optional.of(userDto);
+		
+		ResponseEntity resEntity = new ResponseEntity(user,HttpStatus.OK);
+		assertEquals(resEntity.getStatusCode(),loginController.validWebUser(user).getStatusCode());
 	}
 
 }
